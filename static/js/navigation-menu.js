c9dc28de8ba233232c76f03d29be1ad343fcbe13
taybr713 <taybr713@student.otago.ac.nz>
2024-09-30 09:14:03 +1300

    Fixed homepage and navigation bar;
    - index.html now works and looks normal
    - nav menu loads correctly, although needs to be edited to change depending on if a user is signed in
    
    refs #5
    refs #10

---
+++ static/js/navigation-menu.js
@@ -0,0 +1,13 @@
+export const navigationMenu = {
+    template:
+            `
+            <nav class="navbar">
+                <ul class="nav-menu">
+                    <li><a href="index.html" class="nav-link">Home</a></li>
+                    <li><a href="view-products.html" class="nav-link">View Products</a></li>
+                    <li><a href="sign-in.html" class="nav-link">Sign In</a></li>
+                    <li><a href="create-account.html" class="nav-link">Create an Account</a></li>
+                </ul>
+            </nav>
+		`
+};
