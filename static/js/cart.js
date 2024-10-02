781197a57dc7713d0e9359eb8ba7abed54c2851e
taybr713 <taybr713@student.otago.ac.nz>
2024-10-03 01:30:30 +1300

    Added Vue template to cart page;
    - added a basic template that doesn't work while i messed with the script to get it to work
    
    refs #15

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,54 +1,67 @@
+/* global Vue */
+
 "use strict";
 
+// import data store
+import { sessionStore } from './session-store.js';
+
 class SaleItem {
-	constructor(product, quantityPurchased) {
-		this.product = product;
-		this.quantityPurchased = quantityPurchased;
-		this.salePrice = product.listPrice;
-	}
+    constructor(product, quantityPurchased) {
+        this.product = product;
+        this.quantityPurchased = quantityPurchased;
+        this.salePrice = product.listPrice;
+    }
 }
 
 class Sale {
-	constructor(customer, items) {
-		this.customer = customer;
-		this.items = items;
-	}
+    constructor(customer, items) {
+        this.customer = customer;
+        this.items = items;
+    }
 }
 
 const app = Vue.createApp({
 
     data() {
         return {
-            // models (comma separated key/value pairs)
-
+            cartItems: [], // To hold items from sessionStorage
+            totalAmount: 0 // To hold the total cart amount
         };
     },
 
     computed: Vuex.mapState({
-	    product: 'selectedProduct',
-	    items: 'items',
-	    customer: 'customer'
+        product: 'selectedProduct',
+        items: 'items',
+        customer: 'customer'
     }),
-
 
     mounted() {
         // semicolon separated statements
-
-
+        this.loadCart();
     },
 
     methods: {
         // comma separated function declarations
+        loadCart() {
+            // Retrieve cart items from sessionStorage
+            const items = JSON.parse(sessionStorage.getItem("items")) || [];
+            this.cartItems = items;
 
+            // Calculate the total amount
+            this.totalAmount = this.cartItems.reduce((sum, item) => sum + (item.price * item.quantity), 0);
+        },
+
+        checkOut() {
+            let sale = new Sale(this.customer, this.items);
+            axois.post()
+        }
     }
 
 });
 
 /* other component imports go here */
 
-// import data store
-import { sessionStore } from './session-store.js'
-app.use(sessionStore);	
+app.use(sessionStore);
 
 // mount the page - this needs to be the last line in the file
 app.mount("main");
