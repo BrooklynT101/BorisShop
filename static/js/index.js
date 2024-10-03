ef73c589ec5390f79852fb76e15b81ca2477b38d
taybr713 <taybr713@student.otago.ac.nz>
2024-10-04 08:39:05 +1300

    Added Browser Tab Icon:
    - dynamically changing, cat icon is awake if user is signed in, asleep if user is signed out
    
    closes #27

--- static/js/index.js
+++ static/js/index.js
@@ -7,12 +7,29 @@ import { sessionStore } from './session-store.js';
 import { navigationMenu } from './navigation-menu.js';
 
 // create the Vue controller
-const app = Vue.createApp();
+const app = Vue.createApp({
+    computed: {
+        ...Vuex.mapState({
+                customer: 'customer'
+        })
+    },
+//    had CGPT help me dynamically update the browser icon
+    mounted() {
+        const favicon = document.getElementById("favicon");
+        if (this.customer) {
+            //if signed in cats awake
+            favicon.href = 'images/awake-cat.png';
+        } else {
+            //else it sleeps, hooonk shoooo
+            favicon.href = 'images/sleeping-cat.png';
+        }
+    }
+});
 
 // register the navigation menu under the <navmenu> tag
 app.component('navmenu', navigationMenu);
-
-// sse the Vuex store
+    
+// use the Vuex store
 app.use(sessionStore);
 
 // attach the controller to the <main> tag
